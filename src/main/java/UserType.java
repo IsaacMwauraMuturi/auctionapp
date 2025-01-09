5c34c8ae35413773f87d22e885357fc1ff51b50a
kodhiambo <k.odhiambo@students.jkuat.ac.ke>
2025-01-09 17:43:59 +0300

    Added comments

--- src/main/java/UserType.java
+++ src/main/java/UserType.java
@@ -2,12 +2,23 @@ public enum UserType {
     BUYER("Buyer"),
     ADMIN("Admin"),
     SELLER("Seller");
+
     private final String name;
 
+    /**
+     * Constructor initializes the user type name.
+     * 
+     * @param name The name of the user type.
+     */
     UserType(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the name of the user type.
+     * 
+     * @return The name of the user type.
+     */
     public String getName() {
         return name;
     }
