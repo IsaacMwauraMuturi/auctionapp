deab0f9ea8f5de2df98ea108592d387206be1b55
kodhiambo <k.odhiambo@students.jkuat.ac.ke>
2025-01-09 17:33:58 +0300

    Added comments and refactored

--- src/main/java/ControllerUser.java
+++ src/main/java/ControllerUser.java
@@ -13,7 +13,6 @@ import java.util.Objects;
 
 public class ControllerUser {
     private BeanUser loggedInUser;
-
     private Main mainApplication;
 
     @FXML
@@ -28,17 +27,23 @@ public class ControllerUser {
     @FXML
     private UserDao userDao;
 
-
-
+    /**
+     * Constructor initializes the user DAO.
+     */
     public ControllerUser() {
         userDao = new UserDao(new DatabaseHandler());
     }
 
+    /**
+     * Handles the login process.
+     * 
+     * @throws SQLException if a database access error occurs.
+     * @throws InterruptedException if the thread is interrupted.
+     */
     @FXML
     private void login() throws SQLException, InterruptedException {
         String username = usernameField.getText();
         String password = passwordField.getText();
-
 
         BeanUser user = userDao.getUserByUsername(username);
 
@@ -52,35 +57,45 @@ public class ControllerUser {
         }
     }
 
+    /**
+     * Handles the register button action.
+     * 
+     * @param event The action event.
+     * @throws IOException if an I/O error occurs.
+     */
     @FXML
     void registerButtonHandle(ActionEvent event) throws IOException {
-        try{
+        try {
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Register.fxml")));
             if (root != null) {
                 Scene scene = new Scene(root);
-
                 Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-
                 stage.setScene(scene);
                 stage.show();
-
-
                 ((Stage) usernameField.getScene().getWindow()).close();
             } else {
                 Exception exception = new Exception();
                 exception.printStackTrace();
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Sets the main application instance.
+     * 
+     * @param mainApplication The main application instance.
+     */
     public void setMainApplication(Main mainApplication) {
         this.mainApplication = mainApplication;
     }
 
-
+    /**
+     * Sets the logged-in user.
+     * 
+     * @param user The logged-in user.
+     */
     public void setLoggedInUser(BeanUser user) {
         this.loggedInUser = user;
     }
