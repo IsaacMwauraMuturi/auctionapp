e628ffeb3171c035e583b0d1569a63d57b8355e4
kodhiambo <k.odhiambo@students.jkuat.ac.ke>
2025-01-09 17:10:36 +0300

    Added comments

--- src/main/java/BeanBid.java
+++ src/main/java/BeanBid.java
@@ -1,16 +1,33 @@
 public class BeanBid {
+
     private BeanUser bidder;
     private double amount;
 
+    /**
+     * Constructor initializes the bid details.
+     * 
+     * @param bidder The user placing the bid.
+     * @param amount The amount of the bid.
+     */
     public BeanBid(BeanUser bidder, double amount) {
         this.bidder = bidder;
         this.amount = amount;
     }
 
+    /**
+     * Gets the bidder who placed the bid.
+     * 
+     * @return The bidder.
+     */
     public BeanUser getBidder() {
         return bidder;
     }
 
+    /**
+     * Gets the amount of the bid.
+     * 
+     * @return The bid amount.
+     */
     public double getAmount() {
         return amount;
     }
